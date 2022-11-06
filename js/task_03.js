a825f516a05e0674f8df30561a3099f15c4ae6fb
Mosunov Anton <antonmossunov@gmail.com>
2022-11-06 15:34:23 +0600

    Решение 3-го задания.
    Написан скрипт для решения третьего задания: task_03.js
    Скрипт подключен к странице index.html
    Организован вывод решения в консоль и на главную страницу

---
+++ js/task_03.js
@@ -0,0 +1,13 @@
+const makeDivElement = () => {
+    let out_03 = document.querySelector('.task_03_out')
+    const div = document.getElementById('container')
+    for (let i = 0; i < 5; i++) {
+        const divInDiv = document.createElement('div')
+        div.append(divInDiv)
+        divInDiv.className = 'element'
+        divInDiv.innerText = 'Element' + ' ' + i
+    }
+    console.log(div)
+}
+
+makeDivElement()
