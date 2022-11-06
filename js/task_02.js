a2b6dee2bb8b23167cec788bd5c4e04d76e08b8b
Mosunov Anton <antonmossunov@gmail.com>
2022-11-06 14:51:10 +0600

    Решение 2-го задания.
    Написан скрипт для решения второго задания: task_02.js
    Скрипт подключен к странице index.html
    Организован вывод решения в консоль и на главную страницу

---
+++ js/task_02.js
@@ -0,0 +1,16 @@
+const getElements = () => {
+    const elements = document.getElementsByClassName('element')
+    let out_02 = document.querySelector('.task_02_out')
+    for (let element = 0; element < elements.length; element++) {
+        elements[element].style.color = 'red'
+        if(element < elements.length /2) {
+            elements[element].style.color = 'red'
+            console.log(elements[element].innerText)
+        }
+        else {
+            elements[element].style.color = 'green'
+            console.log(elements[element].innerText)
+        }
+    }
+}
+getElements()
